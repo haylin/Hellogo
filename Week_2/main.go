bd24056ae31f1a7013fd7f72810c324464f8cdef
郭海林 <dev@jqplex.com>
2021-06-06 21:13:46 +0800

    修改提交

--- Week_2/main.go
+++ Week_2/main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func errWarp(uid int) (string, error) {
+	//数据库连接
+	db, err := sql.Open("mysql", "root:123456Aa(127.0.0.1:3306)/geekdb?charset=utf8")
+	if err != nil {
+		fmt.Errorf("访问数据库失败，请检查！")
+	}
+	defer db.Close()
+
+	var (
+		//id int
+		name string
+	)
+
+	err = db.QueryRow("select name from tb_user where uid = ?", uid).Scan(&name)
+	if err != nil {
+		fmt.Println("查询的数据不存在")
+		if err == sql.ErrNoRows {
+			//log.Println("sql.ErrNoRows")
+			return "", fmt.Errorf("%s, 数据为空", err)
+		}
+		log.Fatal(err)
+	}
+	return "", err
+}
+
 func main() {
+	_, err := errWarp(100)
+	err = errors.Unwrap(err)
+	if errors.Is(err, sql.ErrNoRows) {
+		fmt.Println("%s", err)
+	}
 
 }
